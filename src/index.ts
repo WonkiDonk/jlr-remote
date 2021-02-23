9fb19bc9179c28e4964417f0ad45422441ce8509
William Cowell <william.cowell@musclefood.com>
2021-02-23 17:34:19 +0000

    Added module exports

---
+++ src/index.ts
@@ -0,0 +1,23 @@
+import authenticationService from './Authentication/AuthenticationService'
+import commandAuthenticationService from './Authentication/CommandAuthenticationService'
+import queryUserInformationService from './Services/QueryUserInformationService'
+import queryVehicleInformationService from './Services/QueryVehicleInformationService'
+import commandElectricVehicleService from './Services/CommandElectricVehicleService'
+import commandVehicleService from './Services/CommandVehicleService'
+import {AuthenticationService, CommandAuthenticationService} from './Authentication/Services'
+import {QueryUserInformationService, QueryVehicleInformationService, CommandElectricVehicleService, CommandVehicleService} from './Services/Services'
+
+export {
+    authenticationService,
+    commandAuthenticationService,
+    queryUserInformationService,
+    queryVehicleInformationService,
+    commandElectricVehicleService,
+    commandVehicleService,
+    AuthenticationService,
+    CommandAuthenticationService,
+    QueryUserInformationService,
+    QueryVehicleInformationService,
+    CommandElectricVehicleService,
+    CommandVehicleService
+}
