b2ead086c233c068b08e52c489aa8872560a976e
William Cowell <william.cowell@musclefood.com>
2021-02-23 16:58:46 +0000

    Implemented `authenticate`

--- src/Authentication/AuthenticationService.ts
+++ src/Authentication/AuthenticationService.ts
@@ -1,9 +1,19 @@
 
 import { Auth, RefreshTokenResponse } from '../Services/ServiceTypes'
 import { AuthenticationService } from './Services'
+import { baseUrls } from '../Services/ServiceHelpers'
+import axios from 'axios'
+
+const baseUrl = baseUrls.IFAS_BASE_URL
 
 const authenticationService: AuthenticationService = {
-    authenticate: async (deviceId: string, username: string, password: string): Promise<Auth> => { throw new Error('Not implemented') },
+    authenticate: async (deviceId: string, username: string, password: string): Promise<Auth> => {
+        const data = { grant_type: 'password', password, username }
+        const headers = { 'Content-Type': 'application/json', 'Authorization': 'Basic YXM6YXNwYXNz', 'X-Device-Id': deviceId, 'Connection': 'close' }
+        const response = await axios.post(`${baseUrl}/tokens`, data, { headers })
+    
+        return response.data
+    },
 
     refreshToken: async (deviceId: string, refreshToken: string): Promise<RefreshTokenResponse> => { throw new Error('Not implemented') },
 
