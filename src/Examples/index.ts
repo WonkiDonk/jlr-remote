bc474d5380c53db76707fd832e6591000a75a05b
William Cowell <william.cowell@musclefood.com>
2021-02-23 19:04:07 +0000

    Formatting!

--- src/Examples/index.ts
+++ src/Examples/index.ts
@@ -8,7 +8,7 @@ type Car = {
 
 const getElectricVehicles = async (deviceId: string, username: string, password: string): Promise<Car[]> => {
     // Authenticate
-    const {access_token, authorization_token, expires_in } = await jlr_remote.authenticationService.authenticate(deviceId, username, password)
+    const { access_token, authorization_token, expires_in } = await jlr_remote.authenticationService.authenticate(deviceId, username, password)
 
     // Register the application as a device
     await jlr_remote.authenticationService.registerDevice(access_token, deviceId, authorization_token, expires_in, username)
@@ -23,7 +23,7 @@ const getElectricVehicles = async (deviceId: string, username: string, password:
 
     for (const vehicle of userVehicles.vehicles) {
         const vehicleAttributes = await jlr_remote.queryVehicleInformationService.getVehicleAttributes(access_token, deviceId, vehicle.vin)
-        
+
         if (vehicleAttributes.fuelType === 'Electric') {
             electricCars = [...electricCars, {
                 nickname: vehicleAttributes.nickname,
@@ -38,7 +38,7 @@ const getElectricVehicles = async (deviceId: string, username: string, password:
 
 const getBatteryChargeLevel = async (deviceId: string, username: string, password: string, vin: string): Promise<number | null> => {
     // Authenticate
-    const {access_token, authorization_token, expires_in } = await jlr_remote.authenticationService.authenticate(deviceId, username, password)
+    const { access_token, authorization_token, expires_in } = await jlr_remote.authenticationService.authenticate(deviceId, username, password)
 
     // Register the application as a device
     await jlr_remote.authenticationService.registerDevice(access_token, deviceId, authorization_token, expires_in, username)
@@ -48,44 +48,44 @@ const getBatteryChargeLevel = async (deviceId: string, username: string, passwor
 
     const currentVehicleStatus = await jlr_remote.queryVehicleInformationService.getVehicleStatusV3(access_token, deviceId, vin)
     const stateOfCharge = currentVehicleStatus.vehicleStatus?.evStatus.find(status => status.key === 'EV_STATE_OF_CHARGE')
-    
+
     return stateOfCharge?.value
         ? +(stateOfCharge.value)
         : null
 }
 
 const preconditionCar = async (deviceId: string, username: string, password: string, vin: string): Promise<void> => {
-        // Authenticate
-        const {access_token, authorization_token, expires_in } = await jlr_remote.authenticationService.authenticate(deviceId, username, password)
-
-        // Register the application as a device
-        await jlr_remote.authenticationService.registerDevice(access_token, deviceId, authorization_token, expires_in, username)
-    
-        // Login the user
-        const {userId} = await jlr_remote.authenticationService.loginUser(access_token, deviceId, username)
-
-        // Get command token (startClimatePreconditioning requires ECC token)
-        const {token} = await jlr_remote.commandAuthenticationService.getEccToken(access_token, deviceId, vin, userId, vin.slice(vin.length - 4))
-
-        // Send the command to start the preconditioning at 21.0 degrees C
-        await jlr_remote.commandElectricVehicleService.startClimatePrecdonditioning(access_token, deviceId, vin, token, 210)
-}
-
-const lockCar = async (deviceId: string, username: string, password: string, vin: string, userPin: string): Promise<void> => {
     // Authenticate
-    const {access_token, authorization_token, expires_in } = await jlr_remote.authenticationService.authenticate(deviceId, username, password)
+    const { access_token, authorization_token, expires_in } = await jlr_remote.authenticationService.authenticate(deviceId, username, password)
 
     // Register the application as a device
     await jlr_remote.authenticationService.registerDevice(access_token, deviceId, authorization_token, expires_in, username)
 
     // Login the user
-    const {userId} = await jlr_remote.authenticationService.loginUser(access_token, deviceId, username)
+    const { userId } = await jlr_remote.authenticationService.loginUser(access_token, deviceId, username)
+
+    // Get command token (startClimatePreconditioning requires ECC token)
+    const { token } = await jlr_remote.commandAuthenticationService.getEccToken(access_token, deviceId, vin, userId, vin.slice(vin.length - 4))
+
+    // Send the command to start the preconditioning at 21.0 degrees C
+    await jlr_remote.commandElectricVehicleService.startClimatePrecdonditioning(access_token, deviceId, vin, token, 210)
+}
+
+const lockCar = async (deviceId: string, username: string, password: string, vin: string, userPin: string): Promise<void> => {
+    // Authenticate
+    const { access_token, authorization_token, expires_in } = await jlr_remote.authenticationService.authenticate(deviceId, username, password)
+
+    // Register the application as a device
+    await jlr_remote.authenticationService.registerDevice(access_token, deviceId, authorization_token, expires_in, username)
+
+    // Login the user
+    const { userId } = await jlr_remote.authenticationService.loginUser(access_token, deviceId, username)
 
     // Get command token (lock requires ECC token)
-    const {token} = await jlr_remote.commandAuthenticationService.getRdlToken(access_token, deviceId, vin, userId, userPin)
+    const { token } = await jlr_remote.commandAuthenticationService.getRdlToken(access_token, deviceId, vin, userId, userPin)
 
     // Send the command to lock the car.
     await jlr_remote.commandVehicleService.lockVehicle(access_token, deviceId, vin, token)
 }
 
-export default {getElectricVehicles, getBatteryChargeLevel, preconditionCar, lockCar}
+export default { getElectricVehicles, getBatteryChargeLevel, preconditionCar, lockCar }
