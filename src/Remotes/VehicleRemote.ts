b07bb64b8809b2082d1cf402bbf63851cb2641c0
Ian Cooper <ianleecooper@gmail.com>
2021-03-25 17:28:09 +0000

    Refactor constructor - Vehicle Remote

--- src/Remotes/VehicleRemote.ts
+++ src/Remotes/VehicleRemote.ts
@@ -1,14 +1,12 @@
 import {QueryVehicleInformationService} from '../Services/QueryVehicleInformationService'
 
 class VehicleRemote {
-    constructor(private readonly deviceId: string, private readonly vin: string, private readonly service: QueryVehicleInformationService) {
-        this.deviceId = deviceId
-        this.vin = vin
-        this.service = service
+    constructor(private readonly deviceId: string, private readonly vin: string, private readonly queryVehicleInformationService: QueryVehicleInformationService) {
+        
     }
 
     getVehicleAttributes = (): Promise<any> => {
-        return this.service.getVehicleAttributes('', this.deviceId, this.vin,)
+        return this.queryVehicleInformationService.getVehicleAttributes('', this.deviceId, this.vin)
     }
 }
 
