2023cfb42626767ad8aab01b74bffe810052d4fe
William Cowell <william.cowell@musclefood.com>
2021-04-03 18:19:57 +0100

    Documented remote types

--- src/Remotes/Types.ts
+++ src/Remotes/Types.ts
@@ -1,16 +1,28 @@
+/**
+ * Gets access tokens for remote vehicle operations.
+ */
 interface VehicleRemoteAuthenticator {
     getAccessToken: () => Promise<string>
 }
 
+/**
+ * Gets remote vehicle information.
+ */
 interface VehicleRemoteInformation { 
     getVehicleAttributes: () => Promise<any>
     getVehicleStatus: () => Promise<any>
 }
 
+/**
+ * Defines the vehicle lock state.
+ */
 type LockState = {
     isLocked: boolean
 }
 
+/**
+ * Performs common remote vehicle operations.
+ */
 interface VehicleRemoteControl {
     beepAndFlash: () => Promise<void>
     lock(): Promise<void>
@@ -18,6 +30,9 @@ interface VehicleRemoteControl {
     getLockState() : Promise<LockState>
 }
 
+/**
+ * Performs remote operations on vehicles with internal combustion engines (ICEs).
+ */
 interface InternalCombustionEngineVehicleRemoteControl {
     type: 'ICE'
     turnOnClimateControl: (targetTemperature: number) => Promise<void>
@@ -28,12 +43,18 @@ interface InternalCombustionEngineVehicleRemoteControl {
     isEngineOn: () => Promise<boolean>
 }
 
+/**
+ * Defines the EV charge state.
+ */
 type ChargeState = {
     chargeLevel: number
     isCharging: boolean
     isConnected: boolean
 }
 
+/**
+ * Performs remote operations on electric vehicles (EVs).
+ */
 interface ElectricVehicleRemoteControl {
     type: 'EV'
     turnOnClimateControl: (targetTemperature: number) => Promise<void>
@@ -44,14 +65,23 @@ interface ElectricVehicleRemoteControl {
     getChargeState: () => Promise<ChargeState>
 }
 
+/**
+ * Remote for controlling an electric vehicle (EVs).
+ */
 type ElectricVehicleRemote = VehicleRemoteInformation
     & VehicleRemoteControl
     & ElectricVehicleRemoteControl
 
+/**
+ * Remote for controlling a vehicle with an internal combustion engine (ICEs).
+ */
 type InternalCombustionEngineVehicleRemote = VehicleRemoteInformation
     & VehicleRemoteControl
     & InternalCombustionEngineVehicleRemoteControl
 
+/**
+ * Remote for controlling a vehicle.
+ */
 type VehicleRemote = ElectricVehicleRemote | InternalCombustionEngineVehicleRemote
 
 export {
