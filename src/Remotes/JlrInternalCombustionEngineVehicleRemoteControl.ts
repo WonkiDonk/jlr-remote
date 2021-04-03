2c200ccbdbf405f4972e4dd09394bbd78027d811
William Cowell <william.cowell@musclefood.com>
2021-04-03 18:54:43 +0100

    Defined 'JlrInternalCombustionEngineVehicleRemoteControl`

---
+++ src/Remotes/JlrInternalCombustionEngineVehicleRemoteControl.ts
@@ -0,0 +1,31 @@
+import { InternalCombustionEngineVehicleRemoteControl } from './Types'
+
+class JlrInternalCombustionEngineVehicleRemoteControl implements InternalCombustionEngineVehicleRemoteControl {
+    public type: 'ICE' = 'ICE'
+
+    turnOnClimateControl = (targetTemperature: number): Promise<void> => {
+		throw new Error('Not implemented.')
+	}
+
+    turnOffClimateControl = (): Promise<void> => {
+		throw new Error('Not implemented.')
+	}
+
+    isClimateControlOn = (): Promise<boolean> => {
+		throw new Error('Not implemented.')
+	}
+
+    turnOnEngine = (): Promise<void> => {
+		throw new Error('Not implemented.')
+	}
+
+    turnOffEngine = (): Promise<void> => {
+		throw new Error('Not implemented.')
+	}
+
+    isEngineOn = (): Promise<boolean> => {
+		throw new Error('Not implemented.')
+	}
+}
+
+export { JlrInternalCombustionEngineVehicleRemoteControl }
