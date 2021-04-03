3e4c461cf5d948b212b8579e48e4f171d76d8416
William Cowell <william.cowell@musclefood.com>
2021-04-03 18:58:41 +0100

    Tabs -vs- spaces

--- src/Remotes/JlrInternalCombustionEngineVehicleRemoteControl.ts
+++ src/Remotes/JlrInternalCombustionEngineVehicleRemoteControl.ts
@@ -5,27 +5,27 @@ class JlrInternalCombustionEngineVehicleRemoteControl implements InternalCombust
 
     turnOnClimateControl = (targetTemperature: number): Promise<void> => {
 		throw new Error('Not implemented.')
-	}
+    }
 
     turnOffClimateControl = (): Promise<void> => {
 		throw new Error('Not implemented.')
-	}
+    }
 
     isClimateControlOn = (): Promise<boolean> => {
 		throw new Error('Not implemented.')
-	}
+    }
 
     turnOnEngine = (): Promise<void> => {
 		throw new Error('Not implemented.')
-	}
+    }
 
     turnOffEngine = (): Promise<void> => {
 		throw new Error('Not implemented.')
-	}
+    }
 
     isEngineOn = (): Promise<boolean> => {
 		throw new Error('Not implemented.')
-	}
+    }
 }
 
 export { JlrInternalCombustionEngineVehicleRemoteControl }
--- src/Remotes/JlrVehicleRemoteAuthenticator.ts
+++ src/Remotes/JlrVehicleRemoteAuthenticator.ts
@@ -3,7 +3,7 @@ import { VehicleRemoteAuthenticator } from './Types'
 class JlrVehicleRemoteAuthenticator implements VehicleRemoteAuthenticator {
     getAccessToken = (): Promise<string> => {
 		throw new Error('Not implemented.')
-	}
+    }
 }
 
 export { JlrVehicleRemoteAuthenticator }
