fd1f345cc6adde9d3a096af360ae06e6dfde49e6
Ian Cooper <ianleecooper@gmail.com>
2021-04-08 19:22:56 +0100

    JLR Vehicle Remote Control Beep and Flash Uses the access token

--- src/Remotes/JlrVehicleRemoteControl.ts
+++ src/Remotes/JlrVehicleRemoteControl.ts
@@ -1,8 +1,15 @@
-import { LockState, VehicleRemoteControl } from './Types'
+import { LockState, VehicleRemoteAuthenticator, VehicleRemoteControl } from './Types'
+import { CommandVehicleService } from '../Services/CommandVehicleService'
 
 class JlrVehicleRemoteControl implements VehicleRemoteControl {
-    beepAndFlash = (): Promise<void> => {
-        throw new Error('Not implemented.')
+    constructor(private readonly deviceId: string, private readonly vin: string,
+        private readonly vehicleRemoteAuthenticator: VehicleRemoteAuthenticator,
+        private readonly commandVehicleService: CommandVehicleService) { }
+        
+    beepAndFlash = async (): Promise<void> => {
+        const accessToken = await this.vehicleRemoteAuthenticator.getAccessToken()
+
+        this.commandVehicleService.honkHorn(accessToken, '', '', '')
     }
 
     lock = (): Promise<void> => {
--- tests/Remotes/JlrVehicleRemoteControl.test.ts
+++ tests/Remotes/JlrVehicleRemoteControl.test.ts
@@ -1,7 +1,32 @@
+import { JlrVehicleRemoteControl } from "../../src/Remotes/JlrVehicleRemoteControl"
+import { createMock } from 'ts-auto-mock'
+import { CommandVehicleService} from '../../src/Services/CommandVehicleService'
+import { VehicleRemoteAuthenticator } from "../../src/Remotes/Types"
+
 describe('JLR Vehicle Remote Control', () => {
     describe('Beep and flash', () => {
-        test.skip('uses access token `%s`', () => {
+        test.each(['hello world', 'fake token', 'bad token'])
+        ('uses access token `%s`', async (expectedAccessToken: string) => {
+            // Arrange
+            const mockVehicleRemoteAuthentication = createMock<VehicleRemoteAuthenticator>()
+            const mockGetAccessToken = jest.fn()
+            mockGetAccessToken.mockImplementation(() => Promise.resolve(expectedAccessToken))
 
+            mockVehicleRemoteAuthentication.getAccessToken = mockGetAccessToken
+
+            const mockService = createMock<CommandVehicleService>()
+            const remote = new JlrVehicleRemoteControl('', '', mockVehicleRemoteAuthentication, mockService)
+            
+            // Act
+            await remote.beepAndFlash()
+            
+            // Assert
+            expect(mockService.honkHorn).toHaveBeenCalledWith(
+                expectedAccessToken,
+                expect.any(String),
+                expect.any(String),
+                expect.any(String)
+            )
         })
 
         test.skip('uses device ID `%s`', () => {
