e11315c7ba96d57f12da6e6c938127202f156322
Ian Cooper <ianleecooper@gmail.com>
2021-04-08 18:49:32 +0100

    JLR Vehicle Remote Control Beep and Flash Stubbed tests

---
+++ tests/Remotes/JlrVehicleRemoteControl.test.ts
@@ -0,0 +1,53 @@
+describe('JLR Vehicle Remote Control', () => {
+    describe('Beep and flash', () => {
+        test.skip('uses access token `%s`', () => {
+
+        })
+
+        test.skip('uses device ID `%s`', () => {
+
+        })
+
+        test.skip('uses the VIN `%s`', () => {
+
+        })
+
+        describe('Gets hblf command token',() => {
+            test.skip('uses access token `%s`', () => {
+
+            })
+    
+            test.skip('uses device ID `%s`', () => {
+    
+            })
+    
+            test.skip('uses the VIN `%s`', () => {
+    
+            })
+
+            test.skip('uses user ID `%s`', () => {
+
+            })
+
+            test.skip('uses last four of VIN `%s`', () => {
+
+            })
+        })
+
+        test.skip('uses the hblf command token `%s`', () => {
+
+        })
+    })
+
+    describe('Lock vehicle', () => {
+
+    })
+
+    describe('Unlock vehicle', () => {
+
+    })
+
+    describe('Get lock state', () => {
+
+    })
+})
