7830cfe90e7bfd29dd4d64a23b4b05e4077942eb
RubenPuben7 <daniella.fierro7@gmail.com>
2023-03-08 18:54:33 +1100

    JLR Internal Combustion Engine Vehicle Remote Control Turn on Engine - Reon Token uses Access Token - continued + itemised lines in previous tests

--- tests/Remotes/JlrInternalCombustionEngineVehicleRemoteControl.test.ts
+++ tests/Remotes/JlrInternalCombustionEngineVehicleRemoteControl.test.ts
@@ -2,6 +2,7 @@ import { createMock } from 'ts-auto-mock'
 import { CommandIceVehicleService } from '../../src/Services/CommandIceVehicleService'
 import { JlrInternalCombustionEngineVehicleRemoteControl } from '../../src/Remotes/JlrInternalCombustionEngineVehicleRemoteControl'
 import { VehicleRemoteAuthenticator } from '../../src/Remotes/Types'
+import { CommandAuthenticationService, commandAuthenticationService } from '../../src/Authentication/CommandAuthenticationService'
 
 describe('JLR Internal Combustion Engine Vehicle Remote Control', () => {
     describe('Turn on engine', () => {
@@ -12,7 +13,12 @@ describe('JLR Internal Combustion Engine Vehicle Remote Control', () => {
                 mockVehicleRemoteAuthenticator.getAccessToken = jest.fn(() => Promise.resolve(expectedAccessToken))
 
                 const mockCommandIceVehicleService = createMock<CommandIceVehicleService>()
-                const remote = new JlrInternalCombustionEngineVehicleRemoteControl('not important', 'not important', mockVehicleRemoteAuthenticator, mockCommandIceVehicleService)
+                const remote = new JlrInternalCombustionEngineVehicleRemoteControl(
+                    'not important',
+                    'not important', 
+                    mockVehicleRemoteAuthenticator, 
+                    mockCommandIceVehicleService,
+                    createMock<CommandAuthenticationService>())
 
                 // Act
                 await remote.turnOnEngine()
@@ -29,7 +35,12 @@ describe('JLR Internal Combustion Engine Vehicle Remote Control', () => {
             ('uses device ID `%s`', async (expectedDeviceId) => {
                 // Arrange
                 const mockCommandIceVehicleService = createMock<CommandIceVehicleService>()
-                const remote = new JlrInternalCombustionEngineVehicleRemoteControl(expectedDeviceId, 'not important', createMock<VehicleRemoteAuthenticator>(), mockCommandIceVehicleService)
+                const remote = new JlrInternalCombustionEngineVehicleRemoteControl(
+                    expectedDeviceId,
+                    'not important',
+                    createMock<VehicleRemoteAuthenticator>(),
+                    mockCommandIceVehicleService,
+                    createMock<CommandAuthenticationService>())
 
                 // Act
                 await remote.turnOnEngine()
@@ -46,8 +57,13 @@ describe('JLR Internal Combustion Engine Vehicle Remote Control', () => {
             ('uses Vin `%s`', async (expectedVin) => {
 
                 //Arrange
-                const mockCommandIceVehicleService = createMock<CommandIceVehicleService>();
-                const remote = new JlrInternalCombustionEngineVehicleRemoteControl('not important', expectedVin, createMock<VehicleRemoteAuthenticator>(), mockCommandIceVehicleService)
+                const mockCommandIceVehicleService = createMock<CommandIceVehicleService>()
+                const remote = new JlrInternalCombustionEngineVehicleRemoteControl(
+                    'not important',
+                    expectedVin,
+                    createMock<VehicleRemoteAuthenticator>(),
+                    mockCommandIceVehicleService,
+                    createMock<CommandAuthenticationService>())
 
                 //Act
                 await remote.turnOnEngine()
@@ -63,22 +79,75 @@ describe('JLR Internal Combustion Engine Vehicle Remote Control', () => {
 
     describe('Gets REON token', () => {
         test.each(['real token', 'not a real token', 'garbage'])
-            ('uses access token `%s`', async () => {})
-        
+            ('uses access token `%s`', async (expectedAccessToken) => {
+                // Arrange
+                const mockCommandAuthenticationService = createMock<CommandAuthenticationService>()
+                const mockVehicleRemoteAuthenticator = createMock<VehicleRemoteAuthenticator>()
+                mockVehicleRemoteAuthenticator.getAccessToken = jest.fn(() => Promise.resolve(expectedAccessToken))
+                const remote = new JlrInternalCombustionEngineVehicleRemoteControl(
+                    'not important',
+                    'not important',
+                    mockVehicleRemoteAuthenticator,
+                    createMock<CommandIceVehicleService>(),
+                    mockCommandAuthenticationService)
+
+                // Act
+                await remote.turnOnEngine()
+                
+                // Assert
+                expect(mockCommandAuthenticationService.getReonToken).toHaveBeenCalledWith(
+                    expectedAccessToken,
+                    expect.any(String),
+                    expect.any(String),
+                    expect.any(String),
+                    expect.any(String))
+            })
+
+
         test.each(['some device id', 'not a real device id', 'dani and will pairing innit'])
-            ('uses device ID `%s`', async () => { })
+            ('uses device ID `%s`', async (expectedDeviceId) => {
+                // Arrange
+                
+                // Act
+                
+                // Assert
+            })
         
         test.each(['hello world', 'Vin Diesel', 'fake VIN'])
-            ('uses VIN `%s`', async () => { })
+            ('uses VIN `%s`', async (expectedVin) => {
+                // Arrange
+                
+                // Act
+                
+                // Assert
+            })
         
         test.each(['hello world', 'useless ID', 'fake ID'])
-            ('uses user ID `%s`', async () => { })
+            ('uses user ID `%s`', async (expectedUserID) => {
+                // Arrange
+                
+                // Act
+                
+                // Assert
+            })
         
         test.each(['hello world', 'PIN Diesel', 'fake PIN'])
-            ('uses user PIN `%s`', async () => { })
+            ('uses user PIN `%s`', async (expectedUserPin) => {
+                // Arrange
+                
+                // Act
+                
+                // Assert
+            })
 
         test.each(['real token', 'not a real token', 'garbage'])
-            ('uses REON command token `%s`', async () => { })
+            ('uses REON command token `%s`', async (expectedReonCommandToken) => { 
+                // Arrange
+                
+                // Act
+                
+                // Assert
+            })
     })
 
     describe('Turn off engine', () => {})
