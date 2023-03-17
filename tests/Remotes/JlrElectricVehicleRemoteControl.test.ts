cbbaeca41b95329d6aab92783992ae3f19a68baf
RubenPuben7 <daniella.fierro7@gmail.com>
2023-03-17 18:08:12 +1100

    EV Start/Stop Charging - created test file

--- tests/Remotes/JlrElectricVehicleRemoteControl.test.ts
+++ tests/Remotes/JlrElectricVehicleRemoteControl.test.ts
@@ -1,2 +1,15 @@
+describe('JLR Electric Vehicle Remove Control', () => {
+    describe('Start charging', () => {
+        test('placeholder', () => {
 
+        })
+    })
 
+    describe('Stop charging', () => {
+
+    })
+
+    describe('Get charge state', () => {
+
+    })
+})
