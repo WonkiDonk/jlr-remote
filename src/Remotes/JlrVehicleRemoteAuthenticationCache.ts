05e3262a47bb6ded35583a993371e1da447d2e80
William Cowell <wonkidonk@outlook.com>
2023-09-20 08:49:41 +1000

    Refactor for readability

--- src/Remotes/JlrVehicleRemoteAuthenticationCache.ts
+++ src/Remotes/JlrVehicleRemoteAuthenticationCache.ts
@@ -4,12 +4,15 @@ class JlrRemoteAuthenticationCache implements VehicleRemoteAuthenticationCache {
     private timerId?: NodeJS.Timeout
     private accessToken?: string
 
+    private clearCache = () => this.accessToken = undefined
+    private isValidExpiration = (expiresIn: number) => !Number.isNaN(expiresIn) && expiresIn > 0
+
     cacheAuthentication = (accessToken: string, expiresIn: number): void => {
-        if (!Number.isNaN(expiresIn) && expiresIn > 0) {
+        if (this.isValidExpiration(expiresIn)) {
             clearTimeout(this.timerId)
             
             this.accessToken = accessToken
-            this.timerId = setTimeout(() => this.accessToken = undefined, expiresIn * 1000)
+            this.timerId = setTimeout(this.clearCache, expiresIn * 1000)
         }
     }
 
