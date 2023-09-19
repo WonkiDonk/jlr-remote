841ed7ca5fb6b9e31eaf5dbb4b700da2d2c08e97
William Cowell <wonkidonk@outlook.com>
2023-09-19 14:23:58 +1000

    Lint

--- src/Remotes/JlrVehicleRemoteAuthenticator.ts
+++ src/Remotes/JlrVehicleRemoteAuthenticator.ts
@@ -2,15 +2,19 @@ import { VehicleRemoteAuthenticator } from './Types'
 import { AuthenticationService } from "../../src/Authentication/AuthenticationService"
 
 class JlrVehicleRemoteAuthenticator implements VehicleRemoteAuthenticator {
-    constructor(private readonly deviceId: string, private readonly username: string, 
+    constructor(private readonly deviceId: string, private readonly username: string,
         private readonly password: string, private readonly authenticationService: AuthenticationService) { }
 
     getAccessToken = async (): Promise<string> => {
+        return (await this.authenticate()).access_token
+    }
+
+    private async authenticate() {
         const authResponse = await this.authenticationService.authenticate(this.deviceId, this.username, this.password)
         await this.authenticationService.registerDevice(authResponse.access_token, this.deviceId, authResponse.authorization_token, authResponse.expires_in, this.username)
         await this.authenticationService.loginUser(authResponse.access_token, this.deviceId, this.username)
-        
-        return authResponse.access_token
+
+        return authResponse
     }
 }
 export { JlrVehicleRemoteAuthenticator }
--- tests/Remotes/JlrVehicleRemoteAuthenticator.test.ts
+++ tests/Remotes/JlrVehicleRemoteAuthenticator.test.ts
@@ -11,115 +11,122 @@ describe('Vehicle Remote Authenticator', () => {
             ['expectedDeviceId', 'expectedUserName', 'expectedPassword'],
             ['another Device', 'another user name', 'secret password'],
             ['not a real device', 'a fake email', 'a fake password']])
-        ('authenticates with JLR Vehicle Remote', async (expectedDeviceId, expectedUsername, expectedPassword) => {
-            // Arrange
-            const authenticationService = createMock<AuthenticationService>()
-            const builder = new JlrVehicleRemoteAuthenticatorBuilder()
-            builder.authenticationService = authenticationService
-            builder.deviceId = expectedDeviceId
-            builder.username = expectedUsername
-            builder.password = expectedPassword
+            ('authenticates with JLR Vehicle Remote', async (expectedDeviceId, expectedUsername, expectedPassword) => {
+                // Arrange
+                const authenticationService = createMock<AuthenticationService>()
+                const builder = new JlrVehicleRemoteAuthenticatorBuilder()
+                builder.authenticationService = authenticationService
+                builder.deviceId = expectedDeviceId
+                builder.username = expectedUsername
+                builder.password = expectedPassword
 
-            const authenticator = builder.build()
+                const authenticator = builder.build()
             
-            // Act
-            await authenticator.getAccessToken()
+                // Act
+                await authenticator.getAccessToken()
             
-            // Assert
-            expect(authenticationService.authenticate).toHaveBeenCalledWith(
-                expectedDeviceId,
-                expectedUsername,
-                expectedPassword
-            )
-        })
+                // Assert
+                expect(authenticationService.authenticate).toHaveBeenCalledWith(
+                    expectedDeviceId,
+                    expectedUsername,
+                    expectedPassword
+                )
+            })
         test.each([
             ['access token', 'device id', 'auth token', 'expires in', 'user name'],
             ['another token', 'special device id', 'not an auth token', 'never expires in', 'me!'],
             ['final token', 'lol device id', 'simple auth token', 'expires tomorrow', 'your user name']
         ])
-        ('registers the device with JLR Vehicle Remote', async (expectedAccessToken, expectedDeviceId, expectedAuthorizationToken, expectedExpiresIn, expectedUsername) => {
-            // Arrange
-            const authResponse = { access_token: expectedAccessToken, authorization_token: expectedAuthorizationToken, expires_in: expectedExpiresIn, token_type: '', refresh_token: ''}
-            const authenticationService = createMock<AuthenticationService>()
-            authenticationService.authenticate = jest.fn(() => Promise.resolve(authResponse))
+            ('registers the device with JLR Vehicle Remote', async (expectedAccessToken, expectedDeviceId, expectedAuthorizationToken, expectedExpiresIn, expectedUsername) => {
+                // Arrange
+                const authResponse = { access_token: expectedAccessToken, authorization_token: expectedAuthorizationToken, expires_in: expectedExpiresIn, token_type: '', refresh_token: '' }
+                const authenticationService = createMock<AuthenticationService>()
+                authenticationService.authenticate = jest.fn(() => Promise.resolve(authResponse))
 
-            const builder = new JlrVehicleRemoteAuthenticatorBuilder()
-            builder.authenticationService = authenticationService
-            builder.deviceId = expectedDeviceId
-            builder.username = expectedUsername
+                const builder = new JlrVehicleRemoteAuthenticatorBuilder()
+                builder.authenticationService = authenticationService
+                builder.deviceId = expectedDeviceId
+                builder.username = expectedUsername
 
-            const authenticator = builder.build()
+                const authenticator = builder.build()
             
-            // Act
-            await authenticator.getAccessToken()
+                // Act
+                await authenticator.getAccessToken()
             
-            // Assert
-            expect(authenticationService.registerDevice).toHaveBeenCalledWith(
-                expectedAccessToken,
-                expectedDeviceId,
-                expectedAuthorizationToken,
-                expectedExpiresIn,
-                expectedUsername
-            )
-        })
+                // Assert
+                expect(authenticationService.registerDevice).toHaveBeenCalledWith(
+                    expectedAccessToken,
+                    expectedDeviceId,
+                    expectedAuthorizationToken,
+                    expectedExpiresIn,
+                    expectedUsername
+                )
+            })
         test.each([
             ['access token', 'device Id', 'username'],
             ['fake token', 'no device Id', 'fake username'],
             ['bad token', 'bad device', 'no name']
         ])
-        ('login to the JLR Vehicle Remote', async (expectedAccessToken, expectedDeviceId, expectedUsername) => {
-            // Arrange
-            const mockAuth = createMock<Auth>()
-            mockAuth.access_token = expectedAccessToken
+            ('login to the JLR Vehicle Remote', async (expectedAccessToken, expectedDeviceId, expectedUsername) => {
+                // Arrange
+                const mockAuth = createMock<Auth>()
+                mockAuth.access_token = expectedAccessToken
 
-            const authenticationService = createMock<AuthenticationService>()
-            authenticationService.authenticate = jest.fn(() => Promise.resolve(mockAuth))
+                const authenticationService = createMock<AuthenticationService>()
+                authenticationService.authenticate = jest.fn(() => Promise.resolve(mockAuth))
 
-            const builder = new JlrVehicleRemoteAuthenticatorBuilder()
-            builder.authenticationService = authenticationService
-            builder.deviceId = expectedDeviceId
-            builder.username = expectedUsername
+                const builder = new JlrVehicleRemoteAuthenticatorBuilder()
+                builder.authenticationService = authenticationService
+                builder.deviceId = expectedDeviceId
+                builder.username = expectedUsername
 
-            const authenticator = builder.build()
+                const authenticator = builder.build()
             
-            // Act
-            await authenticator.getAccessToken()
+                // Act
+                await authenticator.getAccessToken()
             
-            // Assert
-            expect(authenticationService.loginUser).toHaveBeenCalledWith(
-                expectedAccessToken,
-                expectedDeviceId,
-                expectedUsername
-            )
-        })
+                // Assert
+                expect(authenticationService.loginUser).toHaveBeenCalledWith(
+                    expectedAccessToken,
+                    expectedDeviceId,
+                    expectedUsername
+                )
+            })
 
         test.each(['good token', 'bad token', 'fake token'])
-        ('returns a new access token `%s`', async (expectedAccessToken) => {
-            // Arrange
-            const mockAuth = createMock<Auth>()
-            mockAuth.access_token = expectedAccessToken
+            ('returns a new access token `%s`', async (expectedAccessToken) => {
+                // Arrange
+                const mockAuth = createMock<Auth>()
+                mockAuth.access_token = expectedAccessToken
 
-            const authenticationService = createMock<AuthenticationService>()
-            authenticationService.authenticate = jest.fn(() => Promise.resolve(mockAuth))
+                const authenticationService = createMock<AuthenticationService>()
+                authenticationService.authenticate = jest.fn(() => Promise.resolve(mockAuth))
 
-            const builder = new JlrVehicleRemoteAuthenticatorBuilder()
-            builder.authenticationService = authenticationService
+                const builder = new JlrVehicleRemoteAuthenticatorBuilder()
+                builder.authenticationService = authenticationService
 
-            const authenticator = builder.build()
+                const authenticator = builder.build()
 
-            // Act
-            const response = await authenticator.getAccessToken()
+                // Act
+                const response = await authenticator.getAccessToken()
             
-            // Assert
-            expect(response).toBe(expectedAccessToken)
-        })
+                // Assert
+                expect(response).toBe(expectedAccessToken)
+            })
 
-        test.skip('returns the same access token if the existing access token has not expired', () => {
-            // To do
-        })
+        test.skip
+            ('returns the same access token if the existing access token has not expired', () => {
+                // Todo
+            })
 
-        test.skip('returns a new access token using the refresh token after an access token has expired', () => {
-            // To do
-        })
+        test.skip
+            ('does not re-authenticate if the existing access token has not expired', () => {
+                // To do
+            })
+
+        test.skip
+            ('returns a new access token using the refresh token after an access token has expired', () => {
+                // To do
+            })
     })
 })
